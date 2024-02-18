f0d04e4f6a70585110ccc43c82562f4e40ed159a
Emil Tsanev <emil.tsanev@amusnet.com>
2024-02-18 22:57:21 +0200

    read env variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate dotenv;
+
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+
+    dotenv::from_filename(".env").ok();
+    dotenv::dotenv().ok();
+
+    let descriptor = env::var("WALLET_DESCRIPTOR").unwrap();
+
+    println!("{}", descriptor);
 }
+
